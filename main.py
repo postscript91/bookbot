bbfb9bd4b6b4e65ae67897ceb314441ce201b59c
postscript91 <piotr@slomczykowski.pl>
2025-03-02 19:03:25 +0100

    add sorting and alphanumeric filter to character count

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from stats import count_words
+from stats import count_chars
 
 def main():
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
     word_count = count_words("books/frankenstein.txt")
-    print(f"{word_count} words found in the document")
-
+    char_count = count_chars("books/frankenstein.txt")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
+    for entry in char_count:
+        char = entry["char"]
+        count = entry["count"]
+        if char.isalpha() == True:
+            print(f"{char}: {count}")
+    print("============= END ===============")
 main()
--- stats.py
+++ stats.py
@@ -3,3 +3,24 @@ def count_words(filepath):
         contents = f.read()
         word_count = len(contents.split())
     return word_count
+
+def get_count(item):
+    return item["count"]
+
+def count_chars(filepath):
+    with open(filepath) as f:
+        contents = f.read()
+        contents_lower = contents.lower()
+        char_count = {}
+        for char in contents_lower:
+            if char in char_count:
+                char_count[char] += 1
+            else:
+                char_count[char] = 1
+        sorted_list = []
+        for char, count in char_count.items():
+            sorted_list.append({"char": char, "count": count})
+        sorted_list.sort(key=get_count, reverse=True)
+    return sorted_list
+
+
