7aae254d4fec143db6befd1dafb6399b963b02ac
postscript91 <piotr@slomczykowski.pl>
2025-03-03 17:27:58 +0100

    adding sys.argv and changing main.py to pull from it

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
-from stats import count_words
-from stats import count_chars
+import sys
+from stats import count_words, count_chars
+
+if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
 
 def main():
+    filepath = sys.argv[1]
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {filepath}...")
     print("----------- Word Count ----------")
-    word_count = count_words("books/frankenstein.txt")
-    char_count = count_chars("books/frankenstein.txt")
+    word_count = count_words(filepath)
+    char_count = count_chars(filepath)
     print(f"Found {word_count} total words")
     print("--------- Character Count -------")
     for entry in char_count:
         char = entry["char"]
         count = entry["count"]
-        if char.isalpha() == True:
+        if char.isalpha():
             print(f"{char}: {count}")
     print("============= END ===============")
 main()
